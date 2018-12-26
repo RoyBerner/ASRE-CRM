9ef9e2ea5291b904ee8842d404e52fd55e5f7376
Berner Roy <r.p.berner@me.com>
2018-12-26 22:22:43 +0100

    Initial Commit

---
+++ ASRE CRM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASRE CRM
+//
+//  Created by Roy Berner on 26/12/2018.
+//  Copyright © 2018 Roy Berner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
